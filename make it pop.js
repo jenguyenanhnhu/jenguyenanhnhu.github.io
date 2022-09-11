ac7afe2bd74837a30db897b1dc1e79112e8496d2
jenguyenanhnhu <102585311+jenguyenanhnhu@users.noreply.github.com>
2022-09-11 20:19:14 +1000

    g

--- make it pop.js
+++ make it pop.js
@@ -33,3 +33,35 @@ const skrllr = new Skrllr('main', {
     afterTransition: null
     
 })
+
+// Fade in elements
+
+const callback = function (entries) {
+
+    entries.forEach((entry) => {
+      // console.log(entry);
+        if (entry.isIntersecting) {
+        entry.target.classList.add("animate-fadeIn");
+    }
+    });
+};
+
+const observer = new IntersectionObserver(callback);
+
+const targets = document.querySelectorAll(".js-show-on-scroll");
+targets.forEach(function (target) {
+    target.classList.add("opacity-0");
+    observer.observe(target);
+});
+
+
+  // Fade out "scroll down" arrow
+const target = document.querySelector('#theTarget');
+const arrrow = document.getElementById("scroll-arrow")
+
+document.addEventListener('scroll', () => {
+    if (window.scrollY >= target.getBoundingClientRect().top) {
+        scroll-arrrow.classList.add("animate-fadeOut");
+        scroll-arrrow.classList.add("opacity-0");
+    }
+})
